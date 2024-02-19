31573145c66c5cf946dd2cb1147d8ac935651392
Douglas Rank <douglas.rankjr@gmail.com>
2024-02-18 20:40:01 -0800

    implemented maze class

--- cell.py
+++ cell.py
@@ -6,37 +6,37 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.__x1 = None
-        self.__x2 = None
-        self.__y1 = None
-        self.__y2 = None
-        self.__win = win
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
 
     def draw(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__x2 = x2
-        self.__y1 = y1
-        self.__y2 = y2
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
-            self.__win.draw_line(line, "black")
+            self._win.draw_line(line, "black")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
-            self.__win.draw_line(line, "black")
+            self._win.draw_line(line, "black")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
-            self.__win.draw_line(line, "black")
+            self._win.draw_line(line, "black")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
-            self.__win.draw_line(line, "black")
+            self._win.draw_line(line, "black")
 
     def get_center_coords(self):
-        mid_x = (self.__x1 + self.__x2) / 2
-        mid_y = (self.__y1 + self.__y2) / 2
+        mid_x = (self._x1 + self._x2) / 2
+        mid_y = (self._y1 + self._y2) / 2
         return mid_x, mid_y
 
     def draw_move(self, to_cell, undo=False):
-        if self.__win is None:
+        if self._win is None:
             return
         
         fill_color = "gray" if undo == True else "red"
@@ -46,32 +46,32 @@ class Cell:
         
         # Moves will be rendered in half steps
         # Moving Left
-        if self.__x1 > to_cell.__x1:
-            line = Line(Point(self.__x1, self_y_mid), Point(self_x_mid, self_y_mid))
-            self.__win.draw_line(line, fill_color)
-            line = Line(Point(end_x_mid, end_y_mid), Point(to_cell.__x2, end_y_mid))
-            self.__win.draw_line(line, fill_color)
+        if self._x1 > to_cell._x1:
+            line = Line(Point(self._x1, self_y_mid), Point(self_x_mid, self_y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(end_x_mid, end_y_mid), Point(to_cell._x2, end_y_mid))
+            self._win.draw_line(line, fill_color)
 
         # moving right
-        elif self.__x1 < to_cell.__x1:
-            line = Line(Point(self_x_mid, self_y_mid), Point(self.__x2, self_y_mid))
-            self.__win.draw_line(line, fill_color)
-            line = Line(Point(to_cell.__x1, end_y_mid), Point(end_x_mid, end_y_mid))
-            self.__win.draw_line(line, fill_color)
+        elif self._x1 < to_cell._x1:
+            line = Line(Point(self_x_mid, self_y_mid), Point(self._x2, self_y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_cell._x1, end_y_mid), Point(end_x_mid, end_y_mid))
+            self._win.draw_line(line, fill_color)
 
         # moving up
-        elif self.__y1 > to_cell.__y1:
-            line = Line(Point(self_x_mid, self_y_mid), Point(self_x_mid, self.__y1))
-            self.__win.draw_line(line, fill_color)
+        elif self._y1 > to_cell._y1:
+            line = Line(Point(self_x_mid, self_y_mid), Point(self_x_mid, self._y1))
+            self._win.draw_line(line, fill_color)
             line = Line(Point(end_x_mid, to_cell._y2), Point(end_x_mid, end_y_mid))
-            self.__win.draw_line(line, fill_color)
+            self._win.draw_line(line, fill_color)
 
         # moving down
-        elif self.__y1 < to_cell.__y1:
-            line = Line(Point(self_x_mid, self_y_mid), Point(self_x_mid, self.__y2))
-            self.__win.draw_line(line, fill_color)
-            line = Line(Point(end_x_mid, end_y_mid), Point(end_x_mid, to_cell.__y1))
-            self.__win.draw_line(line, fill_color)
+        elif self._y1 < to_cell._y1:
+            line = Line(Point(self_x_mid, self_y_mid), Point(self_x_mid, self._y2))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(end_x_mid, end_y_mid), Point(end_x_mid, to_cell._y1))
+            self._win.draw_line(line, fill_color)
         
         
         
