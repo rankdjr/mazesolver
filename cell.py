59b44149d9efd26948fe8717562bb767483e3165
Douglas Rank <douglas.rankjr@gmail.com>
2024-02-18 18:47:25 -0800

    added methods to draw path

--- cell.py
+++ cell.py
@@ -29,3 +29,22 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.__win.draw_line(line, "black")
+
+    def get_center_coords(self):
+        mid_x = (self.__x1 + self.__x2) / 2
+        mid_y = (self.__y1 + self.__y2) / 2
+        return mid_x, mid_y
+
+    def draw_move(self, to_cell, undo=False):
+        if self.__win is None:
+            return
+        
+        fill_color = "gray" if undo == True else "red"
+        
+        start_cx, start_cy = self.get_center_coords()
+        end_cx, end_cy = to_cell.get_center_coords()
+        
+        line = Line(Point(start_cx, start_cy), Point(end_cx, end_cy))
+        self.__win.draw_line(line, fill_color)
+        
+        
