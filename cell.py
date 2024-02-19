d62bbd83835d07db0478be29cfcd67a8fa273fde
Douglas Rank <douglas.rankjr@gmail.com>
2024-02-18 21:48:58 -0800

    updated draw to check line_color for existing walls

--- cell.py
+++ cell.py
@@ -17,18 +17,23 @@ class Cell:
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        if self.has_left_wall:
-            line = Line(Point(x1, y1), Point(x1, y2))
-            self._win.draw_line(line, "black")
-        if self.has_right_wall:
-            line = Line(Point(x2, y1), Point(x2, y2))
-            self._win.draw_line(line, "black")
-        if self.has_top_wall:
-            line = Line(Point(x1, y1), Point(x2, y1))
-            self._win.draw_line(line, "black")
-        if self.has_bottom_wall:
-            line = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(line, "black")
+        
+        left_wall = Line(Point(x1, y1), Point(x1, y2))
+        right_wall = Line(Point(x2, y1), Point(x2, y2))
+        top_wall = Line(Point(x1, y1), Point(x2, y1))
+        bottom_wall = Line(Point(x1, y2), Point(x2, y2))
+
+        line_color = "black" if self.has_left_wall == True else "white"
+        self._win.draw_line(left_wall, line_color)
+        
+        line_color = "black" if self.has_right_wall == True else "white"
+        self._win.draw_line(right_wall, line_color)
+        
+        line_color = "black" if self.has_top_wall == True else "white"
+        self._win.draw_line(top_wall, line_color)
+        
+        line_color = "black" if self.has_bottom_wall == True else "white"
+        self._win.draw_line(bottom_wall, line_color)
 
     def get_center_coords(self):
         mid_x = (self._x1 + self._x2) / 2
