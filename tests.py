41e4a0b570771c79be21cd9ed8c8fd96592d591c
Douglas Rank <douglas.rankjr@gmail.com>
2024-02-19 01:15:57 -0800

    added test for graphics classes

--- tests.py
+++ tests.py
@@ -1,33 +1,21 @@
 import unittest
 from maze import Maze
-import random
+from graphics import Window, Point, Line
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
         num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        self.assertEqual(
-            len(m1._cells),
-            num_cols,
-        )
-        self.assertEqual(
-            len(m1._cells[0]),
-            num_rows,
-        )
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
 
     def test_maze_create_cells_large(self):
         num_cols = 16
         num_rows = 12
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        self.assertEqual(
-            len(m1._cells),
-            num_cols,
-        )
-        self.assertEqual(
-            len(m1._cells[0]),
-            num_rows,
-        )
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
 
     def test_maze_reset_cells_visited(self):
         num_cols = 12
@@ -35,10 +23,38 @@ class Tests(unittest.TestCase):
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         for col in m1._cells:
             for cell in col:
-                self.assertEqual(
-                    cell.visited,
-                    False,
-                )
-    
+                self.assertEqual(cell.visited, False)
+
+    def test_create_point(self):
+        point = Point(100, 150)
+        self.assertEqual(point.x, 100)
+        self.assertEqual(point.y, 150)
+
+    def test_create_line(self):
+        p1 = Point(50, 50)
+        p2 = Point(200, 100)
+        line = Line(p1, p2)
+        self.assertEqual(line.p1.x, 50)
+        self.assertEqual(line.p1.y, 50)
+        self.assertEqual(line.p2.x, 200)
+        self.assertEqual(line.p2.y, 100)
+
 if __name__ == "__main__":
-    unittest.main()
+    import sys
+    flags = sys.argv[1:]  # Get command-line arguments excluding the script name
+    if "-m" in flags:
+        # Run only maze-related tests
+        suite = unittest.TestSuite()
+        suite.addTest(Tests("test_maze_create_cells"))
+        suite.addTest(Tests("test_maze_create_cells_large"))
+        suite.addTest(Tests("test_maze_reset_cells_visited"))
+        unittest.TextTestRunner().run(suite)
+    elif "-g" in flags:
+        # Run only graphics-related tests
+        suite = unittest.TestSuite()
+        suite.addTest(Tests("test_create_point"))
+        suite.addTest(Tests("test_create_line"))
+        unittest.TextTestRunner().run(suite)
+    else:
+        # Run all tests
+        unittest.main()
